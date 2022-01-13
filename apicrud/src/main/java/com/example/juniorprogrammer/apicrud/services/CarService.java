658d35d446e875f310435b2f5814c2657cf67d47
abigailvania99 <80343871+abigailvania99@users.noreply.github.com>
2022-01-13 16:10:01 +0700

    Delete apicrud directory

--- apicrud/src/main/java/com/example/juniorprogrammer/apicrud/services/CarService.java
+++
@@ -1,8 +0,0 @@
-package com.example.juniorprogrammer.apicrud.services;
-
-import com.example.juniorprogrammer.apicrud.entities.CarEntity;
-
-public interface CarService {
-
-    CarEntity addCar(CarEntity param);
-}
